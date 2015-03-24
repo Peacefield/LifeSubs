c96789572f590a29dc199dc46ceca08ca8a58346
jeremybosscha <jeremybosscha@gmail.com>
2015-03-24 12:33:32 +0100

    screen size

--- LifeSubs/LifeSubs/Form1.cs
+++ LifeSubs/LifeSubs/Form1.cs
@@ -14,7 +14,11 @@ namespace LifeSubs
     {
         public Form1()
         {
+            var screen = System.Windows.Forms.Screen.PrimaryScreen.Bounds;
+            var width = screen.Width;
+
             InitializeComponent();
+            this.Width = width;
         }
     }
 }
